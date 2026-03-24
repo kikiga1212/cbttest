b3d9519d00e206a3a1d7b831da1c0fdca649fa1d
kant300 <p.o.i.u.0987552@gmail.com>
2026-03-24 12:22:34 +0900

    ExamDTO

---
+++ src/main/java/com/example/cbttest/DTO/ExamDTO.java
@@ -0,0 +1,25 @@
+package com.example.cbttest.DTO;
+
+import com.example.cbttest.Entity.ExamAnswerEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExamDTO {
+    private Long id;
+    private String examineeName;
+    private LocalDateTime examDate;
+    private String status;
+    private Long subjectId;
+    private String subjectName;
+    private Integer totalScore;
+    private Integer maxScore;
+    private List<ExamAnswerEntity> answers;
+}
